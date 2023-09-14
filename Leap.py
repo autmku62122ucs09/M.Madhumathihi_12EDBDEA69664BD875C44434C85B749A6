9aa492cc950737826c5a15f554c7569479df97df
autmku62122ucs09 <144676876+autmku62122ucs09@users.noreply.github.com>
2023-09-14 15:20:14 +0530

    Add files via upload

---
+++ Leap.py
@@ -0,0 +1,10 @@
+def isleapyearyear):
+  if (year % 4==0 and year %100 !=O) or year % 400==0:
+    return true
+else:
+return false
+year =2013
+if is lespear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a lesp year.'.format(year))
